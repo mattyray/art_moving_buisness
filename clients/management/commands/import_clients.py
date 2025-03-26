4b06115069cba2705752f25e883ad35e1e628463
mattyray <mnraynor90@gmail.com>
2025-03-26 11:47:46 -0400

    command structure

--- clients/management/commands/import_clients.py
+++ clients/management/commands/import_clients.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
         csv_path = os.path.join(os.path.dirname(__file__), "clients.csv")
 
         try:
-            df = pd.read_csv(csv_path, encoding='ISO-8859-1')
+            df = pd.read_csv(csv_path, encoding='windows-1252')
 
             created_count = 0
             for _, row in df.iterrows():
