1716e84eec9e9950e4a1455cc38e17826eb5b596
mattyray <mnraynor90@gmail.com>
2025-03-28 15:50:39 -0400

    work order schedule - almosg functional

--- workorders/urls.py
+++ workorders/urls.py
@@ -6,16 +6,14 @@ urlpatterns = [
     path('create/', views.workorder_create, name='workorder_create'),
     path('edit/<int:job_id>/', views.workorder_edit, name='workorder_edit'),
     path('delete/<int:job_id>/', views.workorder_delete, name='workorder_delete'),
-    
+
     path('pending/', views.pending_jobs_view, name='pending_jobs'),
     path('scheduled/', views.scheduled_jobs_view, name='scheduled_jobs'),
     path('completed/', views.completed_jobs_view, name='completed_jobs'),
 
-    path('mark_scheduled/<int:job_id>/', views.mark_scheduled, name='mark_scheduled'),
     path('mark_completed/<int:job_id>/', views.mark_completed, name='mark_completed'),
 
     path('detail/<int:job_id>/', views.workorder_detail, name='workorder_detail'),
-    path('schedule/<int:job_id>/', views.schedule_workorder, name='schedule_workorder'),
 
     # Calendar JSON API
     path("calendar-data/workorders/", views.workorder_calendar_data, name="workorder_calendar_data"),
