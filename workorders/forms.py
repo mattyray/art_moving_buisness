7fe8b8264853e57cca634f42aa6eede02d677098
mattyray <mnraynor90@gmail.com>
2025-05-20 17:18:51 -0400

    Add 'invoiced' field to WorkOrderForm for manual override in edit form

--- workorders/forms.py
+++ workorders/forms.py
@@ -13,29 +13,32 @@ class WorkOrderForm(forms.ModelForm):
         label="Client"
     )
 
+    invoiced = forms.BooleanField(
+        required=False,
+        label="Mark as Invoiced?",
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+
     class Meta:
         model = WorkOrder
         fields = [
             'client',
             'job_description',
             'estimated_cost',
+            'invoiced',
         ]
 
     def __init__(self, *args, **kwargs):
-        super(WorkOrderForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         Client = apps.get_model('clients', 'Client')
         self.fields['client'].queryset = Client.objects.all()
 
-        # Make job_description optional
         self.fields['job_description'].required = False
-
-        # Make estimated_cost optional and style it
         self.fields['estimated_cost'].required = False
         self.fields['estimated_cost'].widget.attrs.update({
             'class': 'form-control',
             'placeholder': '0.00'
         })
-
 
 class EventForm(forms.ModelForm):
     date = forms.DateField(
@@ -45,7 +48,6 @@ class EventForm(forms.ModelForm):
             'placeholder': 'YYYY-MM-DD'
         })
     )
-    # allow blank / add a blank choice so required=False works
     event_type = forms.ChoiceField(
         required=False,
         choices=[('', '---------')] + Event.EVENT_TYPES,
@@ -62,7 +64,6 @@ class EventForm(forms.ModelForm):
         model = Event
         fields = ['event_type', 'address', 'date']
 
-
 EventFormSet = inlineformset_factory(
     WorkOrder,
     Event,
@@ -70,16 +71,6 @@ EventFormSet = inlineformset_factory(
     extra=1,
     can_delete=True
 )
-
-
-EventFormSet = inlineformset_factory(
-    WorkOrder,
-    Event,
-    form=EventForm,
-    extra=1,
-    can_delete=True
-)
-
 
 class JobAttachmentForm(forms.ModelForm):
     file = forms.FileField(required=False)
@@ -87,7 +78,6 @@ class JobAttachmentForm(forms.ModelForm):
     class Meta:
         model = JobAttachment
         fields = ['file']
-
 
 class JobNoteForm(forms.ModelForm):
     note = forms.CharField(required=False, widget=forms.Textarea, label="Note")
