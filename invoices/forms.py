03b81d00d6eb4dfa1610a5faacbf7da52df111b3
mattyray <mnraynor90@gmail.com>
2025-05-21 14:30:04 -0400

    Refactor InvoiceForm for edit-only workflow: remove client selection and streamline to work_order, due_date, amount, status, and notes

--- invoices/forms.py
+++ invoices/forms.py
@@ -1,27 +1,8 @@
 from django import forms
 from .models import Invoice
-from clients.models import Client
 from workorders.models import WorkOrder
 
 class InvoiceForm(forms.ModelForm):
-    client = forms.ModelChoiceField(
-        queryset=Client.objects.all(),  # Use all() so form always validates client ID
-        widget=forms.Select(attrs={
-            'class': 'form-control select2',
-            'style': 'width: 100%;',
-            'placeholder': 'Search for a client...',
-        })
-    )
-    work_order = forms.ModelChoiceField(
-        queryset=WorkOrder.objects.none(),
-        required=False,
-        widget=forms.Select(attrs={
-            'class': 'form-control select2',
-            'style': 'width: 100%;',
-            'placeholder': 'Select a work order...',
-            'disabled': 'disabled',
-        })
-    )
     due_date = forms.DateField(
         widget=forms.DateInput(attrs={
             'class': 'form-control datepicker',
@@ -29,35 +10,18 @@ class InvoiceForm(forms.ModelForm):
         })
     )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        client_id = None
-
-        if 'client' in self.data:
-            client_id = self.data.get('client')
-        elif 'client' in self.initial:
-            client_id = self.initial.get('client')
-        elif self.instance and self.instance.pk:
-            client_id = self.instance.client_id
-
-        try:
-            client_id = int(client_id)
-            self.fields['work_order'].queryset = WorkOrder.objects.filter(
-                client_id=client_id,
-                status='completed'
-            )
-            self.fields['work_order'].widget.attrs.pop('disabled', None)
-        except (TypeError, ValueError):
-            self.fields['work_order'].queryset = WorkOrder.objects.none()
-
     class Meta:
         model = Invoice
         fields = [
-            'client',
             'work_order',
             'due_date',
             'amount',
             'status',
             'notes',
         ]
+        widgets = {
+            'work_order': forms.Select(attrs={'class': 'form-control select2'}),
+            'amount': forms.NumberInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'notes': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
