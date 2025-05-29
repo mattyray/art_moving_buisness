b54e06f7109d456aff8bf613ffd8d7a19f9ac35b
mattyray <mnraynor90@gmail.com>
2025-05-29 16:14:49 -0400

    chore: register InvoiceResource in admin using ImportExportModelAdmin

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Invoice
+from import_export.admin import ImportExportModelAdmin
+from .resources import InvoiceResource
+
+@admin.register(Invoice)
+class InvoiceAdmin(ImportExportModelAdmin):
+    resource_class = InvoiceResource
+    list_display = ('invoice_number', 'client', 'amount', 'status', 'due_date', 'date_created')
+    list_filter = ('status', 'due_date', 'date_created')
+    search_fields = ('invoice_number', 'client__name')
+    ordering = ('-date_created',)
+    date_hierarchy = 'due_date'
+    readonly_fields = ['date_created']
+
 
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
