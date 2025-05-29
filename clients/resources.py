212973e316f849aebf076013338f2fc589aa482b
mattyray <mnraynor90@gmail.com>
2025-05-29 11:26:03 -0400

    Include client ID in import-export CSV

--- clients/resources.py
+++ clients/resources.py
@@ -4,11 +4,10 @@ from .models import Client
 class ClientResource(resources.ModelResource):
     class Meta:
         model = Client
-        import_id_fields = []  # 👈 Don't rely on email or id for import uniqueness
-        fields = ('name', 'email', 'phone', 'address')
+        import_id_fields = []
+        fields = ('id', 'name', 'email', 'phone', 'address')  # 👈 added 'id'
         skip_unchanged = True
         report_skipped = True
 
     def skip_row(self, instance, original, row, import_validation_errors=None):
-        # Skip row if name is completely missing
         return not instance.name
