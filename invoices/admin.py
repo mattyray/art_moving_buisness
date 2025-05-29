5d083e08cf0584e99929e042a3a456f743415785
mattyray <mnraynor90@gmail.com>
2025-05-29 16:52:25 -0400

    fixed admin crash

--- invoices/admin.py
+++ invoices/admin.py
@@ -13,16 +13,6 @@ class InvoiceAdmin(ImportExportModelAdmin):
     date_hierarchy = 'due_date'
     readonly_fields = ['date_created']
 
-
-@admin.register(Invoice)
-class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ('invoice_number', 'client', 'amount', 'status', 'due_date', 'date_created')
-    list_filter = ('status', 'due_date', 'date_created')
-    search_fields = ('invoice_number', 'client__name')
-    ordering = ('-date_created',)
-    date_hierarchy = 'due_date'
-    readonly_fields = ['date_created']
-
     fieldsets = (
         (None, {
             'fields': ('invoice_number', 'client', 'work_order', 'amount', 'status')
