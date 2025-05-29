5aa3285f00a89aa6408d118a8a00c4af9f5236b0
mattyray <mnraynor90@gmail.com>
2025-05-29 16:14:49 -0400

    feat: add InvoiceResource with ForeignKey support for client and work_order

---
+++ invoices/resources.py
@@ -0,0 +1,45 @@
+# invoices/resources.py
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget, DateWidget
+from .models import Invoice
+from clients.models import Client
+from workorders.models import WorkOrder
+
+class InvoiceResource(resources.ModelResource):
+    client = fields.Field(
+        column_name='client_id',
+        attribute='client',
+        widget=ForeignKeyWidget(Client, 'id')
+    )
+    work_order = fields.Field(
+        column_name='work_order_id',
+        attribute='work_order',
+        widget=ForeignKeyWidget(WorkOrder, 'id')
+    )
+    date_created = fields.Field(
+        column_name='date_created',
+        attribute='date_created',
+        widget=DateWidget(format='%Y-%m-%d')
+    )
+    due_date = fields.Field(
+        column_name='due_date',
+        attribute='due_date',
+        widget=DateWidget(format='%Y-%m-%d')
+    )
+
+    class Meta:
+        model = Invoice
+        import_id_fields = ['id']
+        fields = (
+            'id',
+            'client',
+            'work_order',
+            'invoice_number',
+            'date_created',
+            'due_date',
+            'amount',
+            'status',
+            'notes',
+        )
+        skip_unchanged = True
+        report_skipped = True
