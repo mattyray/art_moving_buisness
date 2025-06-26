8550d48fe1ef47bcdc7291175b8c95dcc79fbe67
mattyray <mnraynor90@gmail.com>
2025-06-26 11:59:29 -0400

    Update InvoiceAdmin to remove due_date references
    - Remove due_date from list_display, list_filter, and fieldsets
    - Change date_hierarchy to use date_created instead
    - Remove non-existent updated_at field reference

--- invoices/admin.py
+++ invoices/admin.py
@@ -6,11 +6,11 @@ from .resources import InvoiceResource
 @admin.register(Invoice)
 class InvoiceAdmin(ImportExportModelAdmin):
     resource_class = InvoiceResource
-    list_display = ('invoice_number', 'client', 'amount', 'status', 'due_date', 'date_created')
-    list_filter = ('status', 'due_date', 'date_created')
+    list_display = ('invoice_number', 'client', 'amount', 'status', 'date_created')
+    list_filter = ('status', 'date_created')
     search_fields = ('invoice_number', 'client__name')
     ordering = ('-date_created',)
-    date_hierarchy = 'due_date'
+    date_hierarchy = 'date_created'
     readonly_fields = ['date_created']
 
     fieldsets = (
@@ -18,7 +18,7 @@ class InvoiceAdmin(ImportExportModelAdmin):
             'fields': ('invoice_number', 'client', 'work_order', 'amount', 'status')
         }),
         ('Dates', {
-            'fields': ('due_date', 'date_created', 'updated_at')
+            'fields': ('date_created',)
         }),
         ('Additional Info', {
             'fields': ('notes',),
