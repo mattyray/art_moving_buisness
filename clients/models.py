cfa32a8a2b6da435db98f2b0a14212add7421479
mattyray <mnraynor90@gmail.com>
2025-06-26 10:57:35 -0400

    begin feature implkementation clients/models for billing address

--- clients/models.py
+++ clients/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
-    email = models.EmailField(blank=True, null=True)  # 👈 Allow blanks
+    email = models.EmailField(blank=True, null=True)
     phone = models.CharField(max_length=50, blank=True, null=True)
     address = models.CharField(max_length=255, blank=True, null=True)
+    billing_address = models.CharField(max_length=255, blank=True, null=True)  # ✅ NEW FIELD
 
     def __str__(self):
         return self.name
