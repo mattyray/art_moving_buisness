9e727c66560267914f9c5cf3d649cfc0cc1c11d6
mattyray <mnraynor90@gmail.com>
2025-06-30 14:08:46 -0400

    create users soley thru admin

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SignupPageView, custom_logout
 
 urlpatterns = [
-    path("signup/", SignupPageView.as_view(), name="signup"),
+   # path("signup/", SignupPageView.as_view(), name="signup"),
     path("logout/", custom_logout, name="logout"),
 ]
