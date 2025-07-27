5fbb95eb01e2534e213a18bafc54ebcbb2733644
mattyray <mnraynor90@gmail.com>
2025-07-27 16:50:04 -0400

    urls.py for caLENDDAR APP

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = "calendar_app"
+app_name = 'calendar_app'
 
 urlpatterns = [
-
-    # Week view: click any day in a week to see the whole week
-    # e.g. /calendar/week/04-30-25/
-    path('week/<str:date>/', views.week_detail, name='week_detail'),
-
-    # Day view: e.g. /calendar/day/04-30-25/
-    path('day/<str:date>/', views.day_detail, name='day_detail'),
+    path('week/<str:week_str>/', views.week_detail, name='week_detail'),
+    path('day/<str:day_str>/', views.day_detail, name='day_detail'),
+    path('day/<str:day_str>/update-order/', views.update_daily_order, name='update_daily_order'),
 ]
